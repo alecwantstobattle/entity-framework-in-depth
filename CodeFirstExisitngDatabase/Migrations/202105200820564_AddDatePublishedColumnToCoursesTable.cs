214022d10da11f963a55675b77cb8d5bbd2d4b9b
Alec Tecson <tecsonalr@gmail.com>
2021-05-20 17:12:45 +0800

    Migrations Modifying an Existing Class

---
+++ CodeFirstExisitngDatabase/Migrations/202105200820564_AddDatePublishedColumnToCoursesTable.cs
@@ -0,0 +1,18 @@
+namespace CodeFirstExisitngDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDatePublishedColumnToCoursesTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Courses", "DatePublished", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Courses", "DatePublished");
+        }
+    }
+}
