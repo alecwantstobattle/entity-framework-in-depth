3045e672a337af681f9c21ee42333eaec9a215ac
Alec Tecson <tecsonalr@gmail.com>
2021-05-20 15:58:26 +0800

    Migrations Adding a New Class

--- CodeFirstExisitngDatabase/Course.cs
+++ CodeFirstExisitngDatabase/Course.cs
@@ -21,6 +21,8 @@ namespace CodeFirstExisitngDatabase
 
         public string Description { get; set; }
 
+        public Category Category { get; set; }
+
         public int Level { get; set; }
 
         public float FullPrice { get; set; }
