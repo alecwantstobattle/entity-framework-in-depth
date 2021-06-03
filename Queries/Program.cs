0139ff1d34c0036a442a7fa707fe16e69bdb408d
Alec Tecson <tecsonalr@gmail.com>
2021-06-03 15:49:26 +0800

    Eager Loading

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,7 @@ namespace Queries
         {
             var context = new PlutoContext();
 
-            var courses = context.Courses.ToList();
+            var courses = context.Courses.Include(c => c.Author).ToList();
 
             foreach (var course in courses)
                 Console.WriteLine("{0} by {1}", course.Name, course.Author.Name);
