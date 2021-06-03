608d0bbe0f911fcc6b0ea79bc2cf8da99c2d1fef
Alec Tecson <tecsonalr@gmail.com>
2021-06-03 17:36:14 +0800

    Updating objects

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -11,28 +11,11 @@ namespace Queries
     {
         static void Main(string[] args)
         {
-            //var author = new Author() {Id = 1, Name = "Mosh Hamedani"};
-            
             var context = new PlutoContext();
-            //context.Authors.Attach(author);
 
-            //var authors = context.Authors.ToList();
-
-            //var author = context.Authors.Single(a => a.Id == 1);
-
-            var course = new Course
-            {
-                Name = "New Course",
-                Description = "New Description",
-                FullPrice = 19.95f,
-                Level = 1,
-                // Author = new Author { Id = 1, Name = "Mosh Hamedani" }
-                // Author = author
-                AuthorId = 1
-                // Author = author
-            };
-
-            context.Courses.Add(course);
+            var course = context.Courses.Find(4); // Single(c => c.Id == 4)
+            course.Name = "New Name";
+            course.AuthorId = 2;
 
             context.SaveChanges();
         }
